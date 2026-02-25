3fa0e9775ebc55524b60ef177bc438ef171a5353
dpavlenko <amachulin@iriysoft.ru>
2026-02-25 17:37:38 +0300

    Objective C code fixes

--- project/include/AppodealExtension.h
+++ project/include/AppodealExtension.h
@@ -4,4 +4,12 @@
 #import <Foundation/Foundation.h>
 #import <Appodeal/Appodeal.h>
 
+@interface InitDelegate:NSObject <AppodealInitializationDelegate>
+@end
+
+void logExt(NSString *message);
+
+static BOOL inited = NO;
+static BOOL verboseLog = YES;
+
 #endif
--- project/iphone/AppodealExtension.mm
+++ project/iphone/AppodealExtension.mm
@@ -3,57 +3,53 @@
 #import <Foundation/Foundation.h>
 #import <Appodeal/Appodeal.h>
 
-@interface InitDelegate:NSObject <AppodealInitializationDelegate>
-@end
+  @implementation InitDelegate
+    - (void)appodealSDKDidInitialize {
+      logExt(@"Appodeal SDK inited!");
+      // Appodeal SDK did complete initialization
+      inited = YES;
+      //[self release]
+    }
+  @end // InitDelegate
 
-@implementation InitDelegate
-  - (void)appodealSDKDidInitialize {
-    log(@"Appodeal SDK inited!");
-    // Appodeal SDK did complete initialization
-    inited = YES;
-    //[self release]
+  static BOOL initializing = NO;
+  static NSString *applicationId = nil;
+
+
+  void logExt(NSString *message) {
+    if (verboseLog) NSLog(@"AppodealExt: %@", message);
   }
-@end // InitDelegate
 
-static BOOL inited = NO;
-static BOOL initializing = NO;
-static BOOL verboseLog = YES;
-static NSString *applicationId = nul;
-
-void log(NSString *message) {
-  if (verboseLog) NSLog(@"AppodealExt: %@", message);
-}
 
 namespace appodeal {
-
   int SampleMethod(int inputValue) {
-    NSLog(@"Sample Appodeal!");
+    logExt(@"Sample Appodeal!");
     return inputValue * 100;
   }
 
   void Init(const char *appId) {
     NSString *_appId = [NSString stringWithUTF8String:appId];
-    log(@"Init with ID #@", _appId);
+    logExt([NSString stringWithFormat:@"Init with ID %@", _appId]);
     if (!inited && !initializing) {
       applicationId = _appId;
       initializing = YES;
       AppodealAdType adTypes = AppodealAdTypeInterstitial | AppodealAdTypeRewardedVideo;
-      log(@"Init - started for types #@", adTypes);
+      logExt([NSString stringWithFormat:@"Init - started for types %ld", adTypes]);
 
       [Appodeal setAutocache:YES types:adTypes]; 
       [Appodeal setLogLevel:APDLogLevelVerbose];
 
-      log(@"Init - creating Init delegate...");
+      logExt(@"Init - creating Init delegate...");
 
       // Optional delegate for initialization completion
       [Appodeal setInitializationDelegate:[[InitDelegate alloc] init]];
 
       /// Any other pre-initialization
       /// app specific logic
-      log(@"Init - Appodeal initialize...");
+      logExt(@"Init - Appodeal initialize...");
       [Appodeal initializeWithApiKey:_appId types:adTypes];
     } else {
-      log(@"Init - won't init twice (inited: #i initializing: #i)", inited, initializing);
+      logExt([NSString stringWithFormat:@"Init - won't init twice (inited: %i initializing: %i)", inited, initializing]);
     }
   }
 
